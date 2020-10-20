1a69b85e5b4a172f41b8af7e6866dada54e76fce
Conor Johnson Martin <71397298+ConorJohnsonMartin@users.noreply.github.com>
2020-10-20 19:53:31 -0400

    Add files via upload

---
+++ CD.py
@@ -0,0 +1,13 @@
+# SYSC3010 Lab 5 - CD.py
+#     The file to be jointly edited by Team Members C and D
+
+from C import *
+from D import *
+
+def function_CD():    
+    print ("CD")
+    
+    function_C()
+    function_D()
+
+    
