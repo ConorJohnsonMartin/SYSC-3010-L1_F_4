1c005a5a29aff3d194aaa0e3f051c29606278cc0
Conor Johnson Martin <71397298+ConorJohnsonMartin@users.noreply.github.com>
2020-10-20 20:59:10 -0400

    Update CD.py

--- CD.py
+++ CD.py
@@ -1,11 +1,7 @@
-# SYSC3010 Lab 5 - CD.py
-#     The file to be jointly edited by Team Members C and D
+from C import *
 
-
-
-def function_CD():    
-    print ("CD")
+def function_CD():
+    function_C()
     
-
-
+    print ("CD")
     
