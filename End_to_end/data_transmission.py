61e1220a35c1395ee367b3d2051ed548e96b9e79
Chris Atkinson <56167833+chrisatkinson16@users.noreply.github.com>
2020-11-12 23:02:35 -0500

    Update data_transmission.py

--- End_to_end/data_transmission.py
+++ End_to_end/data_transmission.py
@@ -5,7 +5,7 @@ import http.client
 import time
 
 def transmission(field1 = None, field2= None, field3= None, field4= None, field5= None, field6= None, field7= None, field8= None):
-    key = "LQ1LYNA564IB0EX5"
+    key = "H8QD218BNTIQL7OQ"
     params = urllib.parse.urlencode({'field1': field1, 'field2': field2, 'field3': field3, 'field4': field4, 'field5': field5, 'field6': field6, 'field7': field7, 'field8': field8, 'key':key })
     headers = {"Content-typZZe": "application/x-www-form-urlencoded","Accept": "text/plain"}
     conn = http.client.HTTPConnection("api.thingspeak.com:80")
@@ -32,16 +32,17 @@ def readHeadlessPi():
 
 currentID = readHeadlessPi()['entry_id']
 
-transmission(2, 'test')
+transmission(5, 'test')
 
 broken = 0
 while(True):
     broken +=1
-    if broken == 20:
+    if broken == 200:
         break
     if readHeadlessPi()['entry_id'] == (currentID+1):
         break
     time.sleep(0.1)
 
 print(readHeadlessPi()['field1'])
+print(readHeadlessPi()['field2'])
 time.sleep(1)
