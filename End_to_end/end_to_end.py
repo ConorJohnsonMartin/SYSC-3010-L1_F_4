d4e37a8c281a16b5544ff9e265c1a9b92865b96f
Chris Atkinson <56167833+chrisatkinson16@users.noreply.github.com>
2020-11-13 14:13:16 -0500

    Update end_to_end.py

--- End_to_end/end_to_end.py
+++ End_to_end/end_to_end.py
@@ -4,46 +4,48 @@ import urllib.parse
 import http.client
 import time
 
-#Lab2Part3
+# Lab2Part3
 key = "LQ1LYNA564IB0EX5"
+
+
 def readServerRPi():
-    URL='https://api.thingspeak.com/channels/1155565/feeds.json?api_key=8JDWE6GONX7QWNAR&results=2'
-    KEY='8JDWE6GONX7QWNAR'
-    HEADER='&results=2'
-    NEW_URL=URL+KEY+HEADER
-    get_data=requests.get(NEW_URL).json()
-    channel_id=get_data['channel']['id']
-    field1=get_data['feeds']
-    return (field1)[0]
- 
+    URL = 'https://api.thingspeak.com/channels/1155565/feeds.json?api_key=8JDWE6GONX7QWNAR&results=2'
+    KEY = '8JDWE6GONX7QWNAR'
+    HEADER = '&results=2'
+    NEW_URL = URL + KEY + HEADER
+    get_data = requests.get(NEW_URL).json()
+    channel_id = get_data['channel']['id']
+    field_1 = get_data['feeds']
+    return (field_1)[0]
+
+
 def transmission():
     while True:
         moisture = 1
-        params = urllib.parse.urlencode({'field1': moisture, 'key':key }) 
-        headers = {"Content-typZZe": "application/x-www-form-urlencoded","Accept": "text/plain"}
+        pH = 7.0
+        params = urllib.parse.urlencode({'field1': moisture,'field2': pH, 'key': key})
+        headers = {"Content-typZZe": "application/x-www-form-urlencoded", "Accept": "text/plain"}
         conn = http.client.HTTPConnection("api.thingspeak.com:80")
         try:
             conn.request("POST", "/update", params, headers)
             response = conn.getresponse()
-            print (moisture)
-            print (response.status, response.reason)
+            print(moisture)
+            print(pH)
+            print(response.status, response.reason)
             data = response.read()
             conn.close()
         except:
-            print ("connection failed")
+            print("connection failed")
         break
+
 
 while True:
     currentID = readServerRPi()['entry_id']
     field = readServerRPi()
-    
+
     if field['entry_id'] > (currentID):
         currentID = field['entry_id']
         if field['field1'] == '5':
             transmission()
             print(field['field2'])
-
-
-
-
             
