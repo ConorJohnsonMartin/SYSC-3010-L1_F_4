42474d898fc63789638c28b276e9359275b4c661
Kevin Belanger <56167702+kevin12373@users.noreply.github.com>
2020-11-27 20:22:02 -0500

    Update TestDemo2.py

--- Testing demo/TestDemo2.py
+++ Testing demo/TestDemo2.py
@@ -1,13 +1,34 @@
+# L1-F-4 Water plEase
+# Server RPi code
 # @author Chris Atkinson, Kevin Belanger
+# @version Version 1, 24 November 2020
 
 import requests
 import json
 import urllib.parse
 import http.client
 import time
+import sqlite3
 
-expectedpH = 7  # Values from the database
-plant = "Rose"
+dbconnect = sqlite3.connect("/Users/User/Downloads/SQLiteStudio/plant_database.db");
+dbconnect.row_factory = sqlite3.Row;
+cursor = dbconnect.cursor();
+
+cursor.execute('SELECT * FROM plant_database')
+
+
+id = []
+plantName = []
+expectedMoisture = []
+pHmin = []
+pHmax = []
+
+for row in cursor:
+    id.append(row['ID'])
+    plantName.append(row['Plant Name'])
+    expectedMoisture.append(row['Moisture'])
+    pHmin.append(row['pH min'])
+    pHmax.append(row['pH max'])
 
 #  This function checks that the connection to the server is good. and sends a confirmation message back to the headless RPi
 def transmission(field1 = None, field2= None, field3= None, field4= None, field5= None, field6= None, field7= None, field8= None):
@@ -45,7 +66,7 @@ broken = 0 # This variable stops the while loop from being infinite.
 # This while loop runs until either the timer times out or it gets a reading from the headless RPi.
 while(True):
     broken +=1
-    if broken == 200:  # if there is no connection for 200 seconds then the loop ends.
+    if broken == 2:  # if there is no connection for 200 seconds then the loop ends.
         break
     if readHeadlessPi()['entry_id'] == (currentID+1):  # if the RPi establishes a connection and reads values then the loop stops.
         break
@@ -58,25 +79,25 @@ pH = float (readHeadlessPi()['field2'])
 
 # This function compares the moisture that was found by the sensor and compares it ot the expected moisture which is 1
 # and returns 1 or 0 accordingly.
-def compareMoisture():
-    if (moisture == 1):
+def compareMoisture(j):
+    if (moisture == expectedMoisture[j]):
         return 1  # If the soil is moist return 1.
     else:
         return 0  # If not return 0.
 
 # This function compares the pH to the expected pH value that depends on the plant.
 # Returns 1 if pH is good and 0 if not.
-def comparepH():
-    if(expectedpH - 0.5 < pH) & (pH < expectedpH + 0.5):
+def comparepH(k):
+    if(pHmin[k] < pH) & (pH < pHmax[k]):
         return 1  # If pH is within expected range return 1.
     else:
         return 0  # If not return 0.
 
 # This function checks the compareMoisture() function to see if the plant needs water
 # and returns the message required.
-def giveWater():
+def giveWater(l):
     x = ' '  # Create a local variable that stores the string for later use.
-    if(compareMoisture() == 0):
+    if(compareMoisture(l) == 0):
         x = "Water plEase"  # If compareMoisture() is 0 that means it needs water so the function sets x accordingly.
     else:
         x = "No water needed"  # If compareMoisture() is 1 then no action is required.
@@ -84,9 +105,9 @@ def giveWater():
 
 # This function checks if the comparepH() function has flagged a need for pH supplements or not
 # and returns the message required.
-def givepHSuppliment():
+def givepHSuppliment(m):
     y = ' '
-    if(comparepH() == 0):
+    if(comparepH(m) == 0):
         y = "Give pH suppliment"  # If comparepH() is 0 that means it needs a pH supplement so the function sets y accordingly.
     else:
         y = "pH is sufficient"  # If compareMoisture() is 1 then no action is required.
@@ -94,7 +115,7 @@ def givepHSuppliment():
 
 # Gets user data to set the plant that they have.
 def setPlant():
-    userPlant = plant
+    userPlant = plantName[0]
 
 # prints the current moisture
 def printMoisture():
